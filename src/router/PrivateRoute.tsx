9daa859d678ea994184cf7363c75113d7a6d0ef4
Yead191 <yead191@gmail.com>
2026-06-12 12:28:02 +0600

    fixed

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -3,32 +3,21 @@ import Spinner from "@/components/ui/Spinner";
 import { useGetProfileQuery } from "@/redux/apiSlices/authSlice";
 export function PrivateRoute() {
   const location = useLocation();
-  const token = localStorage.getItem("token");
 
   const {
     data: profile,
     isLoading,
     isFetching,
-    isError,
   } = useGetProfileQuery(null, {
     // skip: !token,
   });
 
-  // useEffect(() => {
-  //   if (isError) {
-  //     toast.error("Please login again");
-  //   }
-  // }, [isError]);
-
-  if (!token) {
-    return <Navigate to="/login" replace state={{ from: location }} />;
-  }
 
   if (isLoading || isFetching) {
     return <Spinner />;
   }
 
-  if (isError || !profile?.data) {
+  if (!profile?.data) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
