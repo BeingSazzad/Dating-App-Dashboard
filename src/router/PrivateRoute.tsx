11f51dfb0b12bc0205dc1e90020bbc08082ff575
Yead191 <yead191@gmail.com>
2026-06-12 12:17:16 +0600

    added vercel json

--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -6,7 +6,7 @@ import { toast } from "sonner";
 export function PrivateRoute() {
   const location = useLocation();
   const { data: profile, isLoading, isFetching, isError } = useGetProfileQuery(null, {
-    skip: !localStorage.getItem("token"),
+    // skip: !localStorage.getItem("token"),
   });
   // console.log(profile)
   if (isLoading || isFetching) {
@@ -15,7 +15,7 @@ export function PrivateRoute() {
   if (isError || !profile?.data) {
     if (isError) {
       // console.log(isError)
-      toast.error("Something went wrong");
+      toast.error("Something went wrong. Please login again!");
     }
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
